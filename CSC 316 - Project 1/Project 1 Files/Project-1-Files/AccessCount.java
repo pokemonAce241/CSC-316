719d4ef45a22a0a07d236f7e4c66c225c60769b1
Mohamad <msaleh@ncsu.edu>
2015-01-24 16:22:21 -0500

    Created classes

---
+++ CSC 316 - Project 1/Project 1 Files/Project-1-Files/AccessCount.java
@@ -0,0 +1,21 @@
+import java.util.Iterator;
+
+
+public class AccessCount extends Heuristic{
+
+	public AccessCount(String inputFileName, String name) {
+		super(inputFileName, name);
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void lookup(String word) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected Iterator result() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
