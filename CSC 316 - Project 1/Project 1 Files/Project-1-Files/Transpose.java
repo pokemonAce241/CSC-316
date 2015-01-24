263112add571159de4b1e086dafddaf24bb8819c
Mohamad <msaleh@ncsu.edu>
2015-01-24 16:22:21 -0500

    Created classes

---
+++ CSC 316 - Project 1/Project 1 Files/Project-1-Files/Transpose.java
@@ -0,0 +1,21 @@
+import java.util.Iterator;
+
+
+public class Transpose extends Heuristic{
+
+	public Transpose(String inputFileName, String name) {
+		super(inputFileName, name);
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void lookup(String word) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected Iterator result() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
