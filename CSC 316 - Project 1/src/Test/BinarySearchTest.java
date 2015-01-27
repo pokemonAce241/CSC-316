cb0d1a1bfe85bbc19156deaccc2b43814a2d3e02
jdbencke <jdbencke@ncsu.edu>
2015-01-27 18:34:09 -0500

    Test classes created

---
+++ CSC 316 - Project 1/src/Test/BinarySearchTest.java
@@ -0,0 +1,34 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BinarySearchTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testLookup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPreProcess() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResult() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBinarySearch() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ CSC 316 - Project 1/src/Test/MoveToFrontTest.java
@@ -0,0 +1,119 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MoveToFrontTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testUpdateList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveToFront() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLookup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResult() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNaiveString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNaiveStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindWord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertNewWord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumberOfComparisons() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumberOfLookups() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareWords() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLookupWordFromFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPreProcess() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPostProcess() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrintResultPrintStream() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrintResultPrintStreamInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrintStatistics() {
+		fail("Not yet implemented");
+	}
+
+}
