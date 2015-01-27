77f3318659946c89b1221f60cd69c28ce09a89de
GAgarwal <GAgarwal@GAgarwal-PC>
2015-01-27 18:46:27 -0500

    Binary Search debugging - check

--- CSC 316 - Project 1/src/BinarySearch.java
+++ CSC 316 - Project 1/src/BinarySearch.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class BinarySearch extends Heuristic {
 
@@ -24,6 +25,7 @@ public class BinarySearch extends Heuristic {
 
 	@Override
 	protected void lookup(String word) {
+		System.out.println(Arrays.toString(list) + "\tAdd: " + word);
 		if (count == 0) {
 			list[count] = new WordWithCount(word);
 			count++;
@@ -86,8 +88,58 @@ public class BinarySearch extends Heuristic {
 	 */
 	@Override
 	public Iterator<WordWithCount> result() {
-		modifySize(count);
-		return Arrays.asList(list).iterator();
+		return new ArrayIterator<WordWithCount>(list);
+	}
+
+	/**
+	 * A simple iterator class for lists. The elements of a list are returned by
+	 * this iterator. No copy of the list is made, so any changes to the list
+	 * are reflected in the iterator.
+	 * 
+	 * @author Michael Goodrich, Eric Zamore, Roberto Tamassia, Matt Stallmann,
+	 *         Specialized for Array Lists by Gitesh Agarwal
+	 */
+	// begin#fragment Iterator
+	public class ArrayIterator<E> implements Iterator<E> {
+
+		private E[] list; // the underlying list
+		private int cursor; // the next position
+
+		/** Creates an element iterator over the given list. */
+		public ArrayIterator(E[] L) {
+			list = L;
+			cursor = (count == 0) ? null : 0;
+		}
+
+		/**
+		 * Returns whether the iterator has a next object.
+		 */
+		public boolean hasNext() {
+			if (cursor < count)
+				return true;
+			else
+				return false;
+		}
+
+		/**
+		 * Returns the next object in the iterator.
+		 */
+		public E next() throws NoSuchElementException {
+			if (!hasNext())
+				throw new NoSuchElementException("No next element");
+			E toReturn = list[cursor];
+			cursor = (cursor == count) ? null : cursor + 1;
+			return toReturn;
+		}
+
+		/**
+		 * Throws an {@link UnsupportedOperationException} in all cases, because
+		 * removal is not a supported operation in this iterator.
+		 */
+		public void remove() throws UnsupportedOperationException {
+			throw new UnsupportedOperationException("remove");
+		}
+
 	}
 
 }
