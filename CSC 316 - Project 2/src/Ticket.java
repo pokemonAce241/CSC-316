4c14f63accc34641c93a6ed99eb2079ab634cf59
GAgarwal <GAgarwal@GAgarwal-PC>
2015-03-17 18:13:11 -0400

    update on main functions

---
+++ CSC 316 - Project 2/src/Ticket.java
@@ -0,0 +1,61 @@
+/**
+ * @author Gitesh Agarwal
+ * 
+ */
+public class Ticket {
+
+	private int id;
+
+	private int priority;
+	
+	private int count;
+
+	public Ticket(int id, int priority) {
+		this.id = id;
+		this.priority = priority;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the priority
+	 */
+	public int getPriority() {
+		return priority;
+	}
+
+	/**
+	 * @param priority the priority to set
+	 */
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
+	/**
+	 * @return the count
+	 */
+	public int getCount() {
+		return count;
+	}
+
+	/**
+	 * @param count the count to set
+	 */
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+}
